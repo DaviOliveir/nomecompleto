53bf6b67308bea94d06ed5a3ceb35708a70e1b28
Davi Jose Batista lima de Oliveira <davibatistalimadeoliveira@gmail.com>
2022-09-16 21:47:47 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string nome, sobrenome;
+
+Console.Write("digite seu nome...");
+
+
+nome = Console.ReadLine()!;
+
+
+Console.Write("digite seu sobrenome..");
+
+
+sobrenome = Console.ReadLine()!;
+
+
+
+Console.WriteLine($"nome completo: {nome} {sobrenome}");
+
+
+
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
